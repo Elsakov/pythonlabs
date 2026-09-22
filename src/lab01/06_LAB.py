88cb33c499b2fff7837fbbe0c71ed2da9c2487e4
Elsakov <daniil51@gmail.com>
2026-09-22 15:19:54 +0300

    лабы

--- src/lab01/06_LAB.py
+++ src/lab01/06_LAB.py
@@ -1,11 +1,11 @@
-n=int(input())
+n=int(input("in_1:"))
 k=0
 c=0
-while n!=0:
-    n1,n2,n3,n4=input().split()
+for i in range(n):
+    n1,n2,n3,n4=input("in_"+ str((i+2))+':').split()
     if n4=="True":
         k+=1
     if n4=="False":
         c+=1
     n=n-1
-print(k,c)
+print("out:", k,c)
